adfaf8c5d6f7f52849824d8b8c4c794908254ca3
edmiraya <edmiraya@users.noreply.github.com>
2019-09-06 13:30:15 -0500

    sw registered

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 window.onload = () => {
   divTitulo.innerHTML = "Hello PWA World";
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then(r => console.log("Service Worker registered"))
+      .catch(console.log);
+  }
 };
